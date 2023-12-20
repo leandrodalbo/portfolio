4beb7c77ff3c36f5bd4138d6b7e4919551d777a1
leandrodalbo <leandro.e.dal.bo@gmail.com>
2023-12-20 05:44:49 +0000

    checking logo and color

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Portfolio() {
-  return <div data-testId="portfolio">Portfolio</div>;
+  return (
+    <div data-testId="portfolio">
+      <h1>main color</h1>
+
+      <h4>sec color</h4>
+    </div>
+  );
 }
 
 export default Portfolio;
